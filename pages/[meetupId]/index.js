fdd4ecdcedcc0c0392b8de3114679bdd54d19485
temiloluwa odumosu <temiloluwaodumosu25@gmail.com>
2021-08-13 17:48:14 +0100

    fixes

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+const MeetupDetail = () => {
+  return (
+    <>
+      <MeetupDetails
+        image="https://drscdn.500px.org/photo/52071370/q%3D80_m%3D1500/v2?sig=9881dcf51dac9e6d1d82eb353f5d7ad8e70593480de02e85cfa52c530e536758"
+        address="5, added asddo adad"
+        title="A first Name"
+        description="Meet up locationss s"
+      />
+    </>
+  );
+};
+
+export default MeetupDetail;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,30 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import React, { useEffect, useState } from 'react'
 
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
-
-function HomePage(props) {
-  return (
-    <Fragment>
-      <Head>
-        <title>Max' Blog</title>
-        <meta
-          name='description'
-          content='I post about programming and web development.'
-        />
-      </Head>
-      <Hero />
-      <FeaturedPosts posts={props.posts} />
-    </Fragment>
-  );
-}
-
-export function getStaticProps() {
-  const featuredPosts = getFeaturedPosts();
-
-  return {
-    props: {
-      posts: featuredPosts,
+import MeetupList from '../components/meetups/MeetupList'
+const DUMMY_MEETUPS = [
+    {
+        id : 'm1',
+        title : 'A First Meetup',
+        image : 'https://drscdn.500px.org/photo/52071370/q%3D80_m%3D1500/v2?sig=9881dcf51dac9e6d1d82eb353f5d7ad8e70593480de02e85cfa52c530e536758',
+        address : 'Some address',
+        description : 'Here is desc'
     },
-  };
+    {
+        id : 'm2',
+        title : 'A Second Meetup',
+        image : 'https://drscdn.500px.org/photo/52071370/q%3D80_m%3D1500/v2?sig=9881dcf51dac9e6d1d82eb353f5d7ad8e70593480de02e85cfa52c530e536758',
+        address : 'Some address for you',
+        description : 'Here is desc'
+    },
+]
+const HomePage = () => {
+   const [loadedMeetups, setLoadedMeetups] = useState([])
+    useEffect(() => {
+        setLoadedMeetups(DUMMY_MEETUPS)
+    },[])
+    return (
+        <MeetupList meetups={loadedMeetups}/>
+    )
 }
 
-export default HomePage;
+export default HomePage
